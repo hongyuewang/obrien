1ddb48df31053b29891c2f98488a4bba02a396dc
Hong Yue Wang <hongyuew26@gmail.com>
2021-02-06 23:30:11 -0500

    Change image source to webcam

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread('solvay_conference.jpg')
+webcam_capture = cv2.VideoCapture(0)
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+while True:
+    _, img = webcam_capture.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    cv2.imshow("O'brien Facial Detection System", img)
 
-cv2.imshow('img', img)
-cv2.waitKey()
+    key = cv2.waitKey(30) & 0xff
+    if key == 27:
+        break
+
+webcam_capture.release()
